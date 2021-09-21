20402114128aa2b389186278af01a4ef7d8177a9
Peter Sysel <peter@sysel.at>
2021-09-21 15:37:29 +0200

    gitignore init

---
+++ Labor_01/src/main/java/cf/soisi/spring_wiederholung/SpringWiederholungApplication.java
@@ -0,0 +1,13 @@
+package cf.soisi.spring_wiederholung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWiederholungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWiederholungApplication.class, args);
+    }
+
+}
---
+++ Labor_01/src/test/java/cf/soisi/spring_wiederholung/SpringWiederholungApplicationTests.java
@@ -0,0 +1,13 @@
+package cf.soisi.spring_wiederholung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWiederholungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
