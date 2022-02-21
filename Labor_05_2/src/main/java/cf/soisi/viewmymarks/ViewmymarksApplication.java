9c2279d5f756ffd8653965dab4fb69088f49b553
Peter Sysel <peter@sysel.at>
2022-02-21 11:47:55 +0100

    Labor 6 init

---
+++ Labor_05_2/src/main/java/cf/soisi/viewmymarks/ViewmymarksApplication.java
@@ -0,0 +1,13 @@
+package cf.soisi.viewmymarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewmymarksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewmymarksApplication.class, args);
+    }
+
+}
---
+++ Labor_06/src/main/java/cf/soisi/personinterest/PersoninterestApplication.java
@@ -0,0 +1,13 @@
+package cf.soisi.personinterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoninterestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersoninterestApplication.class, args);
+    }
+
+}
